bbc0cc123442bde4ddf146a3982afd093fdccdb6
meghana s megha <meghanas303@gmail.com>
2026-06-13 18:16:37 +0530

    update python.py with new code

--- python.py
+++ python.py
@@ -1,1 +1,12 @@
-print("hello" + meghana)
+
+name=manaya
+name=input("enter your name : ")
+enter your name : manaya
+print(name)
+manaya
+age=15
+age=input("enter your age : ")
+print(age)
+15
+print("I am" + name + "my age is" + str(15) )
+i am manaya my age is 15
